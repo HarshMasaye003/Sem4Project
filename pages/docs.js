ca58a988ad96322bf81ccee8b8fe390435fec10b
HarshMasaye06 <vu2s2223003@pvppcoe.ac.in>
2023-03-27 21:39:32 +0530

    image correction

--- pages/docs.js
+++ pages/docs.js
@@ -13,7 +13,7 @@ const docs = () => {
             <div className="p-4 md:w-1/2 w-full">
               <div id='color_cards' className="h-full bg-gray-800 bg-opacity-10 p-8 rounded">
                 {/* <p className="leading-relaxed mb-6">Synth chartreuse iPhone lomo cray raw denim brunch everyday carry neutra before they sold out fixie 90's microdosing. Tacos pinterest fanny pack venmo, post-ironic heirloom try-hard pabst authentic iceland.</p> */}
-                <Image src='/word_240.svg' width='300' height='280' />
+                <Image src='./word_240.svg' width='300' height='280' />
                 <a className="inline-flex items-center">
                   <button class="flex bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
                     Button
@@ -24,7 +24,7 @@ const docs = () => {
             <div className="p-4 md:w-1/2 w-full">
               <div className="h-full bg-gray-800 bg-opacity-10 p-8 rounded">
                 {/* <p className="leading-relaxed mb-6">Synth chartreuse iPhone lomo cray raw denim brunch everyday carry neutra before they sold out fixie 90's microdosing. Tacos pinterest fanny pack venmo, post-ironic heirloom try-hard pabst authentic iceland.</p> */}
-                <Image src='/ppt_240.svg' width='300' height='280' />
+                <Image src='./ppt_240.svg' width='300' height='280' />
                 <a className="inline-flex items-center">
                   <button class="flex bg-transparent hover:bg-orange-500 text-orange-400 font-semibold hover:text-white py-2 px-4 border border-orange-500 hover:border-transparent rounded">
                     Button
